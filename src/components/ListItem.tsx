8c33e25e54600812775e057511fc860b1d7555a9
alan154 <alan@solotrucking.com>
2021-09-24 11:46:08 -0500

    formated ListItem

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -3,13 +3,13 @@ import { addCircleOutline, removeCircleOutline } from 'ionicons/icons';
 import { Item } from '../models'
 
 
-const ListItem: React.FC<{item: Item}> = ({item}) => {
-    return(
+const ListItem: React.FC<{ item: Item }> = ({ item }) => {
+    return (
         <IonItem>
-                { item.sign === '+' ? <IonIcon icon={addCircleOutline}></IonIcon> : <IonIcon icon={removeCircleOutline}></IonIcon>}
-                <IonLabel>{item.title}</IonLabel>
-                <IonLabel>{item.category}</IonLabel>
-                <IonLabel>$ {item.amount.toFixed(2)}</IonLabel>
+            {item.sign === '+' ? <IonIcon icon={addCircleOutline}></IonIcon> : <IonIcon icon={removeCircleOutline}></IonIcon>}
+            <IonLabel>{item.title}</IonLabel>
+            <IonLabel>{item.category}</IonLabel>
+            <IonLabel>$ {item.amount.toFixed(2)}</IonLabel>
         </IonItem>
     )
 }
