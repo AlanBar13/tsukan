03545f83ba711c727d8228d369d8d16a35c48e3e
alan154 <alan@solotrucking.com>
2021-10-03 11:36:15 -0500

    deleted unused dependencies

--- src/components/CategoriesItems.tsx
+++ src/components/CategoriesItems.tsx
@@ -1,4 +1,4 @@
-import { IonLabel, IonItem, IonItemSliding, IonItemOptions, IonItemOption, IonIcon } from '@ionic/react';
+import { IonLabel, IonItem, IonIcon } from '@ionic/react';
 import { closeOutline } from 'ionicons/icons';
 
 const CategoriesItems: React.FC<{ name: string; type: string; deleteItem: (name: string, type: string) => void }> = ({ name, type, deleteItem }) => {
